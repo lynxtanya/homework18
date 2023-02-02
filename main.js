51aaffca8cb5edb51a7629abe56798dd4da9fac7
lynxtanya <122545096+lynxtanya@users.noreply.github.com>
2023-02-02 18:52:49 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const imageArray = [
+    "images/img1.jpg",
+    "images/img2.jpg",
+    "images/img3.jpg",
+    "images/img4.jpg",
+    "images/img5.jpg",
+    "images/img6.jpg",
+    "images/img7.jpg",
+    "images/img8.jpg",
+    "images/img9.jpg"
+];
+
+const image = document.querySelector(".custom-img");
+const button = document.querySelector(".btn-random-image");
+
+window.onload = () => generateRandomPicture(imageArray);
+
+button.addEventListener("click", () => generateRandomPicture(imageArray));
+
+function generateRandomPicture(array){
+	let randomNum = Math.floor(Math.random() * array.length); 
+	image.setAttribute("src", array[randomNum]);
+}
